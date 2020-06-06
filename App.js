8f44e3e211f151c5e34f87b1aada438625ea3eca
huyanhang <ngquanghuy595@live.com>
2020-06-06 12:56:29 +0700

    Chinh sua giao dien Auth Screen va them thanh tab navigation

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
+import React from "react";
+import FirebaseKeys from "./config";
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
-import { creatStackNavigator, createStackNavigator } from "react-navigation-stack";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { Ionicons } from "@expo/vector-icons";
 import LoadingScreen from "./screens/LoadingScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
+
 import HomeScreen from "./screens/HomeScreen";
+import MessageScreen from "./screens/MessageScreen";
+import PostScreen from "./screens/PostScreen";
+import NotificationScreen from "./screens/NotificationScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 
 import * as firebase from "firebase";
+
+//var firebaseConfig = FirebaseKeys;
 
 var firebaseConfig = {
     apiKey: "AIzaSyADRO8_ByZQAi49gthB0YoEUd0tvksHOEg",
@@ -15,13 +26,63 @@ var firebaseConfig = {
     storageBucket: "ccptpm-d3f13.appspot.com",
     messagingSenderId: "126626627526",
     appId: "1:126626627526:web:be945c8c8ecbf3d5987cc7"
-};
+  };
 
 firebase.initializeApp(firebaseConfig);
 
-const AppStack = createStackNavigator({
-    Home: HomeScreen
-});
+const AppTabNavigator = createBottomTabNavigator(
+    {
+        Home: {
+            screen: HomeScreen,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor }) => <Ionicons name="ios-home" size={24} color={tintColor} />
+            }
+        },
+        Message: {
+            screen: MessageScreen,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor }) => <Ionicons name="ios-chatboxes" size={24} color={tintColor} />
+            }
+        },
+        Post: {
+            screen: PostScreen,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor }) => (
+                    <Ionicons
+                        name="ios-add-circle"
+                        size={48}
+                        color="#E9446A"
+                        style={{
+                            shadowColor: "#E9446A",
+                            shadowOffset: { width: 0, height: 10 },
+                            shadowRadius: 10,
+                            shadowOpacity: 0.3
+                        }}
+                    />
+                )
+            }
+        },
+        Notification: {
+            screen: NotificationScreen,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor }) => <Ionicons name="ios-notifications" size={24} color={tintColor} />
+            }
+        },
+        Profile: {
+            screen: ProfileScreen,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor }) => <Ionicons name="ios-person" size={24} color={tintColor} />
+            }
+        }
+    },
+    {
+        tabBarOptions: {
+            activeTintColor: "#161F3D",
+            inactiveTintColor: "#B8BBC4",
+            showLabel: false
+        }
+    }
+);
 
 const AuthStack = createStackNavigator({
     Login: LoginScreen,
@@ -32,7 +93,7 @@ export default createAppContainer(
     createSwitchNavigator(
         {
             Loading: LoadingScreen,
-            App: AppStack,
+            App: AppTabNavigator,
             Auth: AuthStack
         },
         {
