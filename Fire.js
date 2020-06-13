ffa034fef09e60d7ce16199c7e93cde72582097b
QuyenTraan <tranvotoquyen@gmail.com>
2020-06-13 10:50:58 +0700

    fix firebase.default firestore is not function

--- Fire.js
+++ Fire.js
@@ -1,21 +1,14 @@
+import FirebaseKeys from "./config";
 import firebase from "firebase";
+require("firebase/firestore");
 
 class Fire {
     constructor() {
-		var firebaseConfig = {
-			apiKey: "AIzaSyADRO8_ByZQAi49gthB0YoEUd0tvksHOEg",
-			authDomain: "ccptpm-d3f13.firebaseapp.com",
-			databaseURL: "https://ccptpm-d3f13.firebaseio.com",
-			projectId: "ccptpm-d3f13",
-			storageBucket: "ccptpm-d3f13.appspot.com",
-			messagingSenderId: "126626627526",
-			appId: "1:126626627526:web:be945c8c8ecbf3d5987cc7"
-		  };
-        firebase.initializeApp(firebaseConfig);
+        firebase.initializeApp(FirebaseKeys);
     }
 
     addPost = async ({ text, localUri }) => {
-        const remoteUri = await this.uploadPhotoAsync(localUri);
+        const remoteUri = await this.uploadPhotoAsync(localUri, `photos/${this.uid}/${Date.now()}`);
 
         return new Promise((res, rej) => {
             this.firestore
@@ -35,16 +28,14 @@ class Fire {
         });
     };
 
-    uploadPhotoAsync = async uri => {
-        const path = `photos/${this.uid}/${Date.now()}.jpg`;
-
+    uploadPhotoAsync = (uri, filename) => {
         return new Promise(async (res, rej) => {
             const response = await fetch(uri);
             const file = await response.blob();
 
             let upload = firebase
                 .storage()
-                .ref(path)
+                .ref(filename)
                 .put(file);
 
             upload.on(
@@ -59,6 +50,34 @@ class Fire {
                 }
             );
         });
+    };
+
+    createUser = async user => {
+        let remoteUri = null;
+
+        try {
+            await firebase.auth().createUserWithEmailAndPassword(user.email, user.password);
+
+            let db = this.firestore.collection("users").doc(this.uid);
+
+            db.set({
+                name: user.name,
+                email: user.email,
+                avatar: null
+            });
+
+            if (user.avatar) {
+                remoteUri = await this.uploadPhotoAsync(user.avatar, `avatars/${this.uid}`);
+
+                db.set({ avatar: remoteUri }, { merge: true });
+            }
+        } catch (error) {
+            alert("Error: ", error);
+        }
+    };
+
+    signOut = () => {
+        firebase.auth().signOut();
     };
 
     get firestore() {
