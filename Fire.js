ab9b56a5be56861731dccff42d589f807ea3658e
QuyenTraan <tranvotoquyen@gmail.com>
2020-06-13 11:06:26 +0700

    fix loi fire.js

--- Fire.js
+++ Fire.js
@@ -1,10 +1,18 @@
-import FirebaseKeys from "./config";
 import firebase from "firebase";
 require("firebase/firestore");
 
 class Fire {
     constructor() {
-        firebase.initializeApp(FirebaseKeys);
+		var firebaseConfig = {
+			apiKey: "AIzaSyADRO8_ByZQAi49gthB0YoEUd0tvksHOEg",
+			authDomain: "ccptpm-d3f13.firebaseapp.com",
+			databaseURL: "https://ccptpm-d3f13.firebaseio.com",
+			projectId: "ccptpm-d3f13",
+			storageBucket: "ccptpm-d3f13.appspot.com",
+			messagingSenderId: "126626627526",
+			appId: "1:126626627526:web:be945c8c8ecbf3d5987cc7"
+		};
+        firebase.initializeApp(firebaseConfig);
     }
 
     addPost = async ({ text, localUri }) => {
